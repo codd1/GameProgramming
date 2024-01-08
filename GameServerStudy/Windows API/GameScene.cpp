5bd137bc4f87d29c90a3b343f78d4fade046b3cc
CYHONG_PC <mgs08041@naver.com>
2024-01-08 19:20:32 +0900

    오브젝트 설계 #2

--- GameServerStudy/Windows API/GameScene.cpp
+++ GameServerStudy/Windows API/GameScene.cpp
@@ -14,16 +14,27 @@ GameScene::~GameScene()
 
 void GameScene::Init()
 {
+	// Player
 	{
 		Player* player = GET_SINGLE(ObjectManager)->CreateObject<Player>();
 		player->SetPos(Pos{ 400, 400 });
 		GET_SINGLE(ObjectManager)->Add(player);
 	}
+
+	// Monster 5마리
+	for (int32 i = 0; i < 5; i++)
+	{
+		Monster* monster = GET_SINGLE(ObjectManager)->CreateObject<Monster>();
+		monster->SetPos(Pos{ static_cast<float>(i + 1) * 100, 100 });
+		GET_SINGLE(ObjectManager)->Add(monster);
+	}
 }
 
 void GameScene::Update()
 {
-	const vector<Object*>& objects = GET_SINGLE(ObjectManager)->GetObjects();
+	const vector<Object*> objects = GET_SINGLE(ObjectManager)->GetObjects();
+	// 미사일을 쏘면 여기서 에러가 발생하는데,
+	// 벡터를 순회함과 동시에 벡터에 강제적으로 Add 해주고 있어서 문제가 되는 것이다.
 	for (Object* object : objects) {
 		object->Update();
 	}
--- GameServerStudy/Windows API/Missile.cpp
+++ GameServerStudy/Windows API/Missile.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "Missile.h"
 #include "TimeManager.h"
+#include "ObjectManager.h"
 
 Missile::Missile() : Object(ObjectType::Projectile)
 {
@@ -25,8 +26,35 @@ void Missile::Update()
 	_pos.y -= deltaTime * _stat.speed;
 
 	// 충돌
+	const vector<Object*> objects = GET_SINGLE(ObjectManager)->GetObjects();
+	for (Object* object : objects) {
+		if (object == this) {
+			continue;
+		}
+
+		if (object->GetObjectType() != ObjectType::Monster) {
+			continue;
+		}
+
+		Pos p1 = GetPos();
+		Pos p2 = object->GetPos();
+
+		const float dx = p1.x - p2.x;
+		const float dy = p1.y - p2.y;
+		float dist = sqrt(dx * dx + dy * dy);
+
+		if (dist < 25) {	// 누군가 맞았을 경우
+			GET_SINGLE(ObjectManager)->Remove(object);	// 몬스터 제거
+			GET_SINGLE(ObjectManager)->Remove(this);	// 나(미사일) 제거
+			return;
+		}
+	}
 
 	// TODO
+	if (_pos.y < -200) {
+		GET_SINGLE(ObjectManager)->Remove(this);
+		return;
+	}
 }
 
 void Missile::Render(HDC hdc)
