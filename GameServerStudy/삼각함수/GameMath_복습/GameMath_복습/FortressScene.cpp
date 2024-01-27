d479c89880807d19c89e6303ecb19a442ca81986
CYHONG_PC <mgs08041@naver.com>
2024-01-28 04:05:01 +0900

    복습) 포트리스 모작 #2 - 플레이어 턴 시간 설정 (시간이 지나면 턴 바뀜)

--- GameServerStudy/삼각함수/GameMath_복습/GameMath_복습/FortressScene.cpp
+++ GameServerStudy/삼각함수/GameMath_복습/GameMath_복습/FortressScene.cpp
@@ -33,12 +33,15 @@ void FortressScene::Init()
 		Player* player = GET_SINGLE(ObjectManager)->CreateObject<Player>();
 		player->SetPlayerId(1);
 		player->SetPlayerType(PlayerType::CanonTank);
-		player->SetPlayerTurn(true);
+		player->SetPlayerTurn(false);
 
 		GET_SINGLE(ObjectManager)->Add(player);
 
 		player->SetPos(Vector{ 700,400 });
 	}
+
+	// 78. 
+	ChangePlayerTurn();
 }
 
 // 70. FortressScene::Update() 구현
@@ -49,16 +52,62 @@ void FortressScene::Update()
 	for (Object* object : objects) {
 		object->Update();
 	}
+
+	// 79. 턴마다 시간 설정
+	float deltaTime = GET_SINGLE(TimeManager)->GetDeltaTime();
+	_sumTime += deltaTime;
+
+	if (_sumTime >= 1.f) {		// 1초마다 확인
+		_sumTime = 0.f;
+
+		int32 remainTime = GET_SINGLE(UIManager)->GetRemainTime();
+		remainTime = max(0, remainTime - 1);	// 음수 방지
+
+		GET_SINGLE(UIManager)->SetRemainTime(remainTime);
+
+		// 남은 시간이 0이 되면 턴 바꿈
+		if (remainTime == 0) {
+			ChangePlayerTurn();
+		}
+	}
 }
 
 // 73. FortressScene::Render(HDC hdc) 구현
 void FortressScene::Render(HDC hdc)
 {
-	GET_SINGLE(UIManager)->Init();
+	// 정보 UI 출력 안되는 문제 해결
+	// 오타 수정: Init() -> Render(hdc)
+	GET_SINGLE(UIManager)->Render(hdc);
 
 	const vector<Object*> objects = GET_SINGLE(ObjectManager)->GetObjects();
 
 	for (Object* object : objects) {
 		object->Render(hdc);
 	}
+}
+
+void FortressScene::ChangePlayerTurn()
+{
+	_playerTurn = (_playerTurn + 1) % 2;	// 0이었으면 1이 되고, 1이었으면 0이 된다.
+
+	const vector<Object*> objects = GET_SINGLE(ObjectManager)->GetObjects();
+	for (Object* object : objects) {
+		if (object->GetObjectType() != ObjectType::Player) {
+			return;
+		}
+
+		Player* player = static_cast<Player*>(object);
+		if (player->GetPlayerId() == _playerTurn) {
+			player->SetPlayerTurn(true);
+		}
+		else {
+			player->SetPlayerTurn(false);
+		}
+	}
+
+	// Player turn이 바뀌었으므로 나머지 요소들도 초기화 (아래 내용들은 몰라도될듯?)
+	GET_SINGLE(UIManager)->SetRemainTime(10);
+	GET_SINGLE(UIManager)->SetStaminaPercent(100.f);
+	GET_SINGLE(UIManager)->SetPowerPercent(0.f);
+	GET_SINGLE(UIManager)->SetWindPercent(static_cast<float>(-100 + rand() % 200));
 }
--- GameServerStudy/삼각함수/GameMath_복습/GameMath_복습/FortressScene.h
+++ GameServerStudy/삼각함수/GameMath_복습/GameMath_복습/FortressScene.h
@@ -11,5 +11,11 @@ public:
 	virtual void Init() override;
 	virtual void Update() override;
 	virtual void Render(HDC hdc) override;
+
+	// 77. 턴 변경 함수 추가/구현 + 필요한 멤버변수 추가
+	void ChangePlayerTurn();
+private:
+	int32 _playerTurn = 1;
+	float _sumTime = 0.f;
 };
 
