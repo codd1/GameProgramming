3b3c0ffe02e5d04fd6a126664a345427bc92bfb1
CYHONG_PC <mgs08041@naver.com>
2024-03-05 02:38:40 +0900

    멀티스레드 환경에서 CPU의 코드 실행 순서

--- GameServer/Server/Server/Server.cpp
+++ GameServer/Server/Server/Server.cpp
@@ -1,52 +1,59 @@
 ﻿#include <iostream>
 #include <thread>
 #include <vector>
-#include <Windows.h>
+#include <windows.h>
 
 using namespace std;
 
-int buffer[10000][10000];
+int x = 0;
+int y = 0;
+int r1 = 0;
+int r2 = 0;
+
+bool ready = false;
+
+void Thread_1() {
+	while (ready == false){ }
+
+	y = 1;	// Store y
+	// --------------------
+	r1 = x;	// Load x
+
+	// 위에서 y = 1; 과 r1 = x; 는 아예 독립적인 명령이다.
+	// 그렇기 때문에 CPU 마음대로 r1 = x; 명령이 더 연산이 빨리 될 것 같으면 r1 = x; y = 1; 순서로 실행할 수 있게 된다.
+	// CPU가 명령어를 받을 때 무조건 순서대로 처리하지 XXX
+}
+
+void Thread_2() {
+	while (ready == false) { }
+
+	x = 1;	// Store x
+	// --------------------
+	r2 = y;	// Load y
+}
 
 int main()
 {
-    ::memset(buffer, 0, sizeof(buffer));
+	int count = 0;
 
-    {   // 1. sum을 구하는 코드의 시간 체크
-        auto start = GetTickCount64();
+	while (true) {
+		ready = false;
+		count++;
 
-        __int64 sum = 0;
+		x = y = r1 = r2 = 0;
 
-        for (int i = 0; i < 10000; i++) {
-            for (int j = 0; j < 10000; j++) {
-                sum += buffer[i][j];
-            }
-        }
+		thread t1(Thread_1);
+		thread t2(Thread_2);
 
-        auto end = GetTickCount64();
+		ready = true;
 
-        cout << "Elapsed Tick " << (end - start) << endl;
-    }
+		t1.join();
+		t2.join();
 
-    {   // 2. sum을 구하는 코드의 시간 체크
-        auto start = GetTickCount64();
+		if (r1 == 0 && r2 == 0) {
+			break;
+		}
+	}
 
-        __int64 sum = 0;
-
-        for (int i = 0; i < 10000; i++) {
-            for (int j = 0; j < 10000; j++) {
-                sum += buffer[j][i];        // 이 부분만 다름
-            }
-        }
-
-        auto end = GetTickCount64();
-
-        cout << "Elapsed Tick " << (end - start) << endl;
-        /*
-            실행 결과
-            Elapsed Tick 78
-            Elapsed Tick 406
-
-            약 5~6배의 시간 차이가 난다. --> 즉, 캐시가 있다!
-        */
-    }
+	cout << count << endl;
 }
