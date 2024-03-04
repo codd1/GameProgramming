150d9bcc9d63ac5a7a74392381561711b235b01c
CYHONG_PC <mgs08041@naver.com>
2024-03-05 03:11:23 +0900

    공유 자원 사용의 문제점 및 해결방안(atomic)

--- GameServer/Server/Server/Server.cpp
+++ GameServer/Server/Server/Server.cpp
@@ -2,34 +2,41 @@
 #include <thread>
 #include <vector>
 #include <windows.h>
+#include <atomic>
 
 using namespace std;
 
-int x = 0;
-int y = 0;
-int r1 = 0;
-int r2 = 0;
+//int sum = 0;
+atomic<int>sum = 0;
 
-bool ready = false;
-// volatile bool ready = false;
-
-void Thread_1() {
-	ready = false;
-
-	while (ready == false) {
-
+void Add() {
+	for (int i = 0; i < 1000000; i++) {		// 100만 번
+		sum++;	// atomic 사용 이후 -> All or Nothing (실행이 되거나 안되거나 둘 중 하나. 중간에 다른게 끼어들 수 X)
+		
+		//// 어셈블리어로 확인해보면 sum++;은 3단계로 이루어짐 (atomic 사용 전)
+		//int eax = sum;		// 0
+		//eax = sum + 1;		// 0 -> 1
+		//sum = eax;			// sum <- 1
 	}
+}
 
-	cout << "Yeah!!!" << endl;
+void Sub() {
+	for (int i = 0; i < 1000000; i++) {
+		sum--;
+
+		//int eax = sum;		// 0
+		//eax = sum - 1;		// 0 -> -1
+		//sum = eax;			// sum <- -1
+	}
 }
 
 int main()
 {
-	thread t1(Thread_1);
-
-	this_thread::sleep_for(1s);
-
-	ready = true;
+	thread t1(Add);
+	thread t2(Sub);
 
 	t1.join();
+	t2.join();
+
+	cout << sum << endl;
 }
