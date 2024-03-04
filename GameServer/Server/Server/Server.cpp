4505b2867f7a0c76e7a2a78351316e3b1eb8a17c
CYHONG_PC <mgs08041@naver.com>
2024-03-05 02:49:23 +0900

    멀티스레드 환경에서 CPU의 코드 실행 순서 - 주석추가

--- GameServer/Server/Server/Server.cpp
+++ GameServer/Server/Server/Server.cpp
@@ -11,49 +11,25 @@ int r1 = 0;
 int r2 = 0;
 
 bool ready = false;
+// volatile bool ready = false;
 
 void Thread_1() {
-	while (ready == false){ }
+	ready = false;
 
-	y = 1;	// Store y
-	// --------------------
-	r1 = x;	// Load x
+	while (ready == false) {
 
-	// 위에서 y = 1; 과 r1 = x; 는 아예 독립적인 명령이다.
-	// 그렇기 때문에 CPU 마음대로 r1 = x; 명령이 더 연산이 빨리 될 것 같으면 r1 = x; y = 1; 순서로 실행할 수 있게 된다.
-	// CPU가 명령어를 받을 때 무조건 순서대로 처리하지 XXX
-}
+	}
 
-void Thread_2() {
-	while (ready == false) { }
-
-	x = 1;	// Store x
-	// --------------------
-	r2 = y;	// Load y
+	cout << "Yeah!!!" << endl;
 }
 
 int main()
 {
-	int count = 0;
+	thread t1(Thread_1);
 
-	while (true) {
-		ready = false;
-		count++;
+	this_thread::sleep_for(1s);
 
-		x = y = r1 = r2 = 0;
+	ready = true;
 
-		thread t1(Thread_1);
-		thread t2(Thread_2);
-
-		ready = true;
-
-		t1.join();
-		t2.join();
-
-		if (r1 == 0 && r2 == 0) {
-			break;
-		}
-	}
-
-	cout << count << endl;
+	t1.join();
 }
