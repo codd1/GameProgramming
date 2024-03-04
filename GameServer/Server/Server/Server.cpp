d451c4e29d135a0396118c2384b88d0c367a2a5e
CYHONG_PC <mgs08041@naver.com>
2024-03-05 02:03:55 +0900

    캐시 확인 코드 - 주석 추가

--- GameServer/Server/Server/Server.cpp
+++ GameServer/Server/Server/Server.cpp
@@ -11,7 +11,7 @@ int main()
 {
     ::memset(buffer, 0, sizeof(buffer));
 
-    {
+    {   // 1. sum을 구하는 코드의 시간 체크
         auto start = GetTickCount64();
 
         __int64 sum = 0;
@@ -27,19 +27,26 @@ int main()
         cout << "Elapsed Tick " << (end - start) << endl;
     }
 
-    {
+    {   // 2. sum을 구하는 코드의 시간 체크
         auto start = GetTickCount64();
 
         __int64 sum = 0;
 
         for (int i = 0; i < 10000; i++) {
             for (int j = 0; j < 10000; j++) {
-                sum += buffer[j][i];
+                sum += buffer[j][i];        // 이 부분만 다름
             }
         }
 
         auto end = GetTickCount64();
 
         cout << "Elapsed Tick " << (end - start) << endl;
+        /*
+            실행 결과
+            Elapsed Tick 78
+            Elapsed Tick 406
+
+            약 5~6배의 시간 차이가 난다. --> 즉, 캐시가 있다!
+        */
     }
 }
