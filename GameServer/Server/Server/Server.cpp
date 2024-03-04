ddf35d43536beb73edad0cf04cf3516edccb9aeb
CYHONG_PC <mgs08041@naver.com>
2024-03-05 03:20:15 +0900

    atomic 관련 함수

--- GameServer/Server/Server/Server.cpp
+++ GameServer/Server/Server/Server.cpp
@@ -6,32 +6,32 @@
 
 using namespace std;
 
-//int sum = 0;
 atomic<int>sum = 0;
 
 void Add() {
 	for (int i = 0; i < 1000000; i++) {		// 100만 번
-		sum++;	// atomic 사용 이후 -> All or Nothing (실행이 되거나 안되거나 둘 중 하나. 중간에 다른게 끼어들 수 X)
-		
-		//// 어셈블리어로 확인해보면 sum++;은 3단계로 이루어짐 (atomic 사용 전)
-		//int eax = sum;		// 0
-		//eax = sum + 1;		// 0 -> 1
-		//sum = eax;			// sum <- 1
+		sum++;	// atomic 사용 이후 -> All or Nothing
 	}
 }
 
 void Sub() {
 	for (int i = 0; i < 1000000; i++) {
 		sum--;
-
-		//int eax = sum;		// 0
-		//eax = sum - 1;		// 0 -> -1
-		//sum = eax;			// sum <- -1
 	}
 }
 
 int main()
 {
+	//int temp = sum;
+	int temp = sum.load();	// 위와 같은 의미인데, load함수를 쓰면 atomic하게 가져온다는 의미가 직관적으로 보임.
+
+	//sum = 10;
+	sum.store(10);	// 위와 같은 의미, 직관적
+
+	int temp = sum.exchange(10);	// sum에 10을 저장하기 전, sum의 값. 단순히 temp라고 생각할 수 있지만 저장되기 전에 다른 값이 들어올 수 있다는 것도 항상 염두에 둬야 함.
+
+	int prev = sum.fetch_add(10);	// sum에 10을 저장한 후, 10을 저장하기 전의 값
+
 	thread t1(Add);
 	thread t2(Sub);
 
