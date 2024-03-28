8ac293c5fc547691a993d9f134873445433c8a02
홍채영 <mgs08041@naver.com>
2024-03-29 04:54:41 +0900

    git merge 문제?

---
+++ GameServer/Server/Server/Server.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+void HelloThread(int i) {
+	while (true) {
+		cout << "Hello Thread" << i << endl;
+	}
+}
+
+int main() {
+	vector<thread> threads;
+
+	for (int i = 0; i < 10; i++) {
+		threads.push_back(thread(HelloThread, i));
+	}
+
+	cout << "Hello Main" << endl;
+
+	for (thread& t : threads) {
+		t.join();
+	}
+
+	while (true) {
+
+	}
+}
